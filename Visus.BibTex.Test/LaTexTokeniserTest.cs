5db1e18de56de99d543d3d7bf0379c60633014cd
Christoph Müller <christoph.mueller@visus.uni-stuttgart.de>
2024-06-20 11:14:38 +0200

    Tests for trivial LaTex tokenisation.

---
+++ Visus.BibTex.Test/LaTexTokeniserTest.cs
@@ -0,0 +1,109 @@
+﻿// <copyright file="LaTexTokeniserTest.cs" company="Visualisierungsinstitut der Universität Stuttgart">
+// Copyright © 2024 Visualisierungsinstitut der Universität Stuttgart.
+// Licensed under the MIT licence. See LICENCE file for details.
+// </copyright>
+// <author>Christoph Müller</author>
+
+
+namespace Visus.BibTex.Test {
+
+    /// <summary>
+    /// Tests LaTex tokenisation.
+    /// </summary>
+    [TestClass]
+    public class LaTexTokeniserTest {
+
+        [TestMethod]
+        public void TestSingleToken() {
+            {
+                var input = "\\";
+                var tokeniser = new LaTexTokeniser(input);
+                Assert.AreEqual(LaTexTokenType.Backslash, tokeniser.Next().Type);
+                Assert.AreEqual(LaTexTokenType.End, tokeniser.Next().Type);
+            }
+
+            {
+                var input = "{";
+                var tokeniser = new LaTexTokeniser(input);
+                Assert.AreEqual(LaTexTokenType.BraceLeft, tokeniser.Next().Type);
+                Assert.AreEqual(LaTexTokenType.End, tokeniser.Next().Type);
+            }
+
+            {
+                var input = "}";
+                var tokeniser = new LaTexTokeniser(input);
+                Assert.AreEqual(LaTexTokenType.BraceRight, tokeniser.Next().Type);
+                Assert.AreEqual(LaTexTokenType.End, tokeniser.Next().Type);
+            }
+
+            {
+                var input = "$";
+                var tokeniser = new LaTexTokeniser(input);
+                Assert.AreEqual(LaTexTokenType.Dollar, tokeniser.Next().Type);
+                Assert.AreEqual(LaTexTokenType.End, tokeniser.Next().Type);
+            }
+
+            {
+                var input = "";
+                var tokeniser = new LaTexTokeniser(input);
+                Assert.AreEqual(LaTexTokenType.End, tokeniser.Next().Type);
+            }
+
+            {
+                var input = "b";
+                var tokeniser = new LaTexTokeniser(input);
+                Assert.AreEqual(LaTexTokenType.Literal, tokeniser.Next().Type);
+                Assert.AreEqual(LaTexTokenType.End, tokeniser.Next().Type);
+            }
+
+            {
+                var input = "bla";
+                var tokeniser = new LaTexTokeniser(input);
+                Assert.AreEqual(LaTexTokenType.Literal, tokeniser.Next().Type);
+                Assert.AreEqual(LaTexTokenType.End, tokeniser.Next().Type);
+            }
+
+            {
+                var input = "\n";
+                var tokeniser = new LaTexTokeniser(input);
+                Assert.AreEqual(LaTexTokenType.NewLine, tokeniser.Next().Type);
+                Assert.AreEqual(LaTexTokenType.End, tokeniser.Next().Type);
+            }
+
+            {
+                var input = "\"";
+                var tokeniser = new LaTexTokeniser(input);
+                Assert.AreEqual(LaTexTokenType.Quote, tokeniser.Next().Type);
+                Assert.AreEqual(LaTexTokenType.End, tokeniser.Next().Type);
+            }
+
+            {
+                var input = " ";
+                var tokeniser = new LaTexTokeniser(input);
+                Assert.AreEqual(LaTexTokenType.WhiteSpace, tokeniser.Next().Type);
+                Assert.AreEqual(LaTexTokenType.End, tokeniser.Next().Type);
+            }
+
+            {
+                var input = "\t";
+                var tokeniser = new LaTexTokeniser(input);
+                Assert.AreEqual(LaTexTokenType.WhiteSpace, tokeniser.Next().Type);
+                Assert.AreEqual(LaTexTokenType.End, tokeniser.Next().Type);
+            }
+
+            {
+                var input = "\r";
+                var tokeniser = new LaTexTokeniser(input);
+                Assert.AreEqual(LaTexTokenType.WhiteSpace, tokeniser.Next().Type);
+                Assert.AreEqual(LaTexTokenType.End, tokeniser.Next().Type);
+            }
+
+            {
+                var input = " \r\t\r ";
+                var tokeniser = new LaTexTokeniser(input);
+                Assert.AreEqual(LaTexTokenType.WhiteSpace, tokeniser.Next().Type);
+                Assert.AreEqual(LaTexTokenType.End, tokeniser.Next().Type);
+            }
+        }
+    }
+}
